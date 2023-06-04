d70a05b45734ba155e7c1eb0b5a7848239317f12
yohrran <johnyohan93@gmail.com>
2023-06-05 00:53:29 +0900

    add test case and eslint setting

--- meadowlark.js
+++ meadowlark.js
@@ -2,33 +2,27 @@ const express = require("express");
 const { engine } = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
-const fortune = require("./lib/fortune");
+const handlers = require("./lib/handler");
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
 app.set("views", "./views");
 app.use(express.static(__dirname + "/public"));
 
-app.get("/", (req, res) => {
-  res.render("home");
-});
+app.get("/", handlers.home);
 
-app.get("/about", (req, res) => {
-  res.render("about", { fortune: fortune.getFortune });
-});
+app.get("/about", handlers.about);
 
-app.use((req, res) => {
-  res.status(404);
-  res.render("404");
-});
+app.use(handlers.notFound);
 
-app.use((err, req, res, next) => {
-  res.status(500);
-  res.render("500");
-});
+app.use(handlers.serverError);
 
-app.listen(port, () => {
-  console.log(
-    `Express started on http://localhost:${port} + press Ctrl + C to terminate`
-  );
-});
+if (require.main === module) {
+  app.listen(port, () => {
+    console.log(
+      `Express started on http://localhost:${port} + press Ctrl + C to terminate`
+    );
+  });
+} else {
+  module.exports = app;
+}
