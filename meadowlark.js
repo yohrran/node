9e7fc1b786278bcb354e9062ce7b9011f797ad4c
yohrran <johnyohan93@gmail.com>
2023-06-06 14:38:07 +0900

    add handlebars form

--- meadowlark.js
+++ meadowlark.js
@@ -3,6 +3,10 @@ const { engine } = require("express-handlebars");
 const app = express();
 const port = process.env.PORT || 3000;
 const handlers = require("./lib/handler");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.disable("x-powered-by");
 
 app.engine("handlebars", engine());
 app.set("view engine", "handlebars");
@@ -12,6 +16,36 @@ app.use(express.static(__dirname + "/public"));
 app.get("/", handlers.home);
 
 app.get("/about", handlers.about);
+
+app.get("/greeting", (req, res) => {
+  res.render("greeting", {
+    message: "Hello esteemed programmer!",
+    style: req.query.style,
+    userid: req.cookies.userid,
+    username: req.session.username,
+  });
+});
+
+app.get("/no-layout", (req, res) => {
+  res.render("no-layout", { layout: null });
+});
+
+app.get("/custom/layout", (req, res) => {
+  res.render("custom-layout", { layout: "custom" });
+});
+
+app.get("/text", (req, res) => {
+  res.type("text/plain");
+  res.send("this is a test");
+});
+
+app.get("/headers", (req, res) => {
+  res.type("text/plain");
+  const headers = Object.entries(req.headers).map(
+    ([key, value]) => `${key}: ${value}`
+  );
+  res.send(headers.join("\n"));
+});
 
 app.use(handlers.notFound);
 
